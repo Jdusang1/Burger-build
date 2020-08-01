19aa55aba7b4d8f3cb30e3768f7d7e392eeabf81
JohnDusang <Johndusang1@gmail.com>
2020-07-31 18:57:56 -0600

    forgot to do commits but updated routes, handlebar files, and controllers

--- controllers/burgersController.js
+++ controllers/burgersController.js
@@ -3,9 +3,50 @@ var express = require("express");
 var router = express.Router();
 
 // Import the model (cat.js) to use its database functions.
-var cat = require("../models/burger.js");
+var burger = require("../models/burger.js");
 
+router.get("/", function (req, res) {
+    burger.all(function (data) {
+        res.render("index", { burgers: data });
+    });
+});
 
+router.post("/api/burgers", function (req, res) {
+    burger.create([
+        "burger_name", "devoured"
+    ], [
+        req.body.burger_name, req.body.devoured
+    ], function (result) {
+        res.json({ id: result.insertId })
+    });
+});
+
+router.put("/api/burgers/:id", function (req, res) {
+    let condition = "id = " + req.params.id;
+
+    burger.update({
+        devoured: req.body.devoured
+
+    }, condition, function (result) {
+        if (result.changedRows == 0) {
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    });
+});
+
+router.delete("/api/burgers/:id", function (req, res) {
+    let condition = "id = " + req.params.id;
+
+    burger.delete(condition, function (result) {
+        if (result.affectedRows == 0) {
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    });
+});
 
 
 module.exports = router;
---
+++ public/assets/js/burgers.js
@@ -0,0 +1,72 @@
+
+$(function () {
+    $(".change-devoured").on("click", function (event) {
+        var id = $(this).data("id");
+        var newDevoured = $(this).data("newdevoured");
+
+        var newDevouredState = {
+            devoured: newDevoured
+        };
+
+        // Send the PUT request.
+        $.ajax("/api/burgers/" + id, {
+            type: "PUT",
+            data: newDevouredState
+        }).then(
+            function () {
+                console.log("changed sleep to", newDevoured);
+                // Reload the page to get the updated list
+                location.reload();
+            }
+        );
+    });
+
+    $(".create-form").on("submit", function (event) {
+        // Make sure to preventDefault on a submit event.
+        event.preventDefault();
+
+        var newBurger = {
+            burger_name: $("#devoured").val().trim(),
+            devoured: $("[name=devoured]:checked").val().trim()
+        };
+
+        // Send the POST request.
+        $.ajax("/api/burgers", {
+            type: "POST",
+            data: newBurger
+        }).then(
+            function () {
+                console.log("created new burger");
+                // Reload the page to get the updated list
+                location.reload();
+            }
+        );
+    });
+
+    $(".delete-burger").on("click", function (event) {
+        var id = $(this).data("id");
+
+        // Send the DELETE request.
+        $.ajax("/api/burgers/" + id, {
+            type: "DELETE"
+        }).then(
+            function () {
+                console.log("deleted burger", id);
+                // Reload the page to get the updated list
+                location.reload();
+            }
+        );
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+
